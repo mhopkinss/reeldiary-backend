afcdedb1017ed8bb2f133b12cd3998b35ec7912f
Marcos Hopkins <marcos123hopkins@gmail.com>
2022-09-29 14:34:11 -0700

    test

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.use((req, res, next) => {
 })
 
 //routes
+app.use('/', function(req, res){
+    res.send('WORKING!')
+})
 app.use('/movies', movieRoutes)
 app.use('/categories', categoryRoutes)
 app.use('/user', userRoutes)
